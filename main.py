342d1852130a4c1593fd47f7422af22f09076760
Jhosua Saut Maruli <80184758+KZREVOXTICAL@users.noreply.github.com>
2022-09-20 22:05:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,44 +3,88 @@ import sys
 import json
 import requests
 import argparse
+from concurrent.futures import ThreadPoolExecutor
 
 # Color
 G = "\033[1;32m"
 R = "\033[1;91m"
 W = "\033[1;97m"
+B = "\033[1;94m"
+
+
+banner = """{} _____                 _____ _ _   
+|  _  |___ ___ _ _ _ _|  |  |_| |_ 
+|   __|  _| . |_'_| | |    -| |  _|
+|__|  |_| |___|_,_|_  |__|__|_|_|  
+                  |___|    {}
+
+{} - {}Author    : {}Jhosua
+{} - {}Facebook  : {}https://www.facebook.com/kzdgzr{}
+""".format(B,W,G,W,G,G,W,G,W)
+
 
 URL_BASE = "https://api.proxyscrape.com/v2/?request=proxyinfo&protocol={}&timeout={}&country=all&ssl=all&anonymity=all&simplified=true"
 URL_DOWN = "https://api.proxyscrape.com/v2/?request=getproxies&protocol={}&timeout={}&country=all&ssl=all&anonymity=all&simplified=true"
 
 
-def getProxy(tproxy,pproxy):
-     raw = requests.get(URL_BASE.format(tproxy,pproxy))
-     total = json.loads(raw.text)["proxy_count"]
-     if(total == 0):
-          print("{}[{}!{}] Proxy Not Found!".format(W,R,W))
-     else:
-          print("{}[{}!{}] Proxies found as many as {}{}{}".format(W,G,W,G,total,W))
-          r = requests.get(URL_DOWN.format(tproxy,pproxy))
+def checkerProxy(listProxy,output):
+     try:
+          fo = open(output,'a')
+          for proxyList in listProxy.splitlines():
+               with open(output,'a') as f:
+                    r = requests.post("https://api.proxyscrape.com/v2/online_check.php",data={"ip_addr[]":[proxyList]})
+                    data = json.loads(r.text)[0]
+                    if(data["working"] == True):
+                         print(f"{W}[{data['country']}{W}] [{G}LIVE{W}] {proxyList}")
+                         f.write(str(proxyList) + "\n")
+                    else:
+                         print(f"{W}[UN] [{R}DEAD{W}] {proxyList}")
+     except:
+          pass
+
+
+def dumpProxy(protocol,out):
+     r = requests.get(URL_BASE.format(protocol,out)).json()
+     if(r["proxy_count"] != 0 ):
+          print("{}[{}!{}] Proxies found as many as {}{}{}".format(W,G,W,G,r["proxy_count"],W))
+          raw = requests.get(URL_DOWN.format(protocol,out))
           output = input("{}[{}-{}] Save As : ".format(W,G,W))
-          if os.path.exists(output):
-               with open(output,mode="a") as f: f.write(r.text)
-          
-          if not os.path.exists(output):
-               with open(output,mode="w") as f: f.write(r.text)
-          
-          print("{}[{}!{}] Successfully saved as {}{}{}".format(W,G,W,G,output,W))
+          iFile = open(output,'a')
+          with open(output,'a') as f:
+               f.write(str(raw.text))
+
+     else:
+          print("{}[{}!{}] Proxy Not Found!".format(W,R,W))
+
 
 
 if __name__ == '__main__':
-     parser = argparse.ArgumentParser(description="Proxy Downloader\n\nCreated By Jhosua\n\n")
-     parser.add_argument('-t',type=str,help="Enter type proxy http,socks4,socks5")
-     parser.add_argument('-p',type=int,help="Enter Time Respone Exm : 200")
+     os.system("clear")
+     print(banner)
+     parser = argparse.ArgumentParser(description="ProxyKit")
+     parser.add_argument('-p',type=str,help="Proxy Protocols, Exam : http,socks4,socks5")
+     parser.add_argument('-t',type=int,help="Timeout Proxy")
+     parser.add_argument('-f',type=str,help="List file proxy")
+     parser.add_argument('-o',type=str,help="Output file")
+
      args = parser.parse_args()
+
+     with ThreadPoolExecutor(max_workers=30) as th:
+          if(args.p != None and args.t != None and args.f == None):
+               if(args.p in ["http","socks4","socks5"]):
+                    th.submit(dumpProxy,args.p,args.t)
+               else:
+                    print(f"[{R}!{W}] Incorrect Proxy Protocol")
      
-     if(args.t == None):
-          print(f"Sorry the format you entered is wrong, \nplease enter the command: python {sys.argv[0]} -h\n\nUsage examples: python3 {sys.argv[0]} -t socks4 -p 300")
-     else:
-          if args.t in ["http","socks4","socks5"]:
-               getProxy(args.t,args.p)
+          elif(args.f != None and args.t == None and args.p == None):
+               if(args.o != None):
+                    try:
+                         listProxy = open(args.f).read()
+                         th.submit(checkerProxy,listProxy,args.o)
+                    except FileNotFoundError:
+                         print(f"[{R}!{W}] File not found!")
+               else:
+                    print(f"[{R}!{W}] Enter output file!")
+
           else:
-               print("The supported proxy types are http,socks4 and socks5")
+               print(f"- Download Proxy: {sys.argv[0]} -p socks4 -t 300\n- Checking Proxy: {sys.argv[0]} -f listproxy.txt -o output.txt\n\n")
